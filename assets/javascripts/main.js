16fd2cf6649846bc35b72ccacb29652a30a13294
Trevor Saint <hello@trevorsaint.com>
2014-09-05 12:35:56 +0100

    Updated details/summary polyfill. Added page to demonstrate a working version

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,57 +1,122 @@
-$(document).ready(function() {
+function showHideCheckboxToggledContent() {
 
+  $(".block-label input[type='checkbox']").each(function() {
 
-	/* FORM FOCUS STYLES */
+    var $checkbox = $(this);
+    var $checkboxLabel = $(this).parent();
 
-	if ($('.form').length>0) {
+    var $dataTarget = $checkboxLabel.attr('data-target');
 
-	$(".block-label").each(function() {
+    // Add ARIA attributes
 
-		// Add focus
+    // If the data-target attribute is defined
+    if (typeof $dataTarget !== 'undefined' && $dataTarget !== false) {
 
-		$(".block-label input").focus(function() {
+      // Set aria-controls
+      $checkbox.attr('aria-controls', $dataTarget);
 
-			$("label[for='" + this.id + "']").addClass("add-focus");
+      // Set aria-expanded and aria-hidden
+      $checkbox.attr('aria-expanded', 'false');
+      $('#'+$dataTarget).attr('aria-hidden', 'true');
 
-		}).blur(function() {
+      // For checkboxes revealing hidden content
+      $checkbox.on('click', function() {
 
-			$("label").removeClass("add-focus");
+        var state = $(this).attr('aria-expanded') === 'false' ? true : false;
 
-		});
+        // Toggle hidden content
+        $('#'+$dataTarget).toggle();
 
-		// Add selected class
+        // Update aria-expanded and aria-hidden attributes
+        $(this).attr('aria-expanded', state);
+        $('#'+$dataTarget).attr('aria-hidden', !state);
 
-		$('input:checked').parent().addClass('selected');
+      });
+    }
 
-	});
+  });
 
-	// Add/remove selected class
+}
 
-	$('.block-label').find('input[type=radio], input[type=checkbox]').click(function() {
+function showHideRadioToggledContent() {
 
-		$('input:not(:checked)').parent().removeClass('selected');
-		$('input:checked').parent().addClass('selected');
+  $(".block-label input[type='radio']").each(function() {
 
-		$('.toggle-content').hide();
+    var $radio = $(this);
+    var $radioGroupName = $(this).attr('name');
+    var $radioLabel = $(this).parent();
 
-		var target = $('input:checked').parent().attr('data-target');
+    var $dataTarget = $radioLabel.attr('data-target');
 
-		$('#'+target).show();
+    // Add ARIA attributes
 
-	});
+    // If the data-target attribute is defined
+    if (typeof $dataTarget !== 'undefined' && $dataTarget !== false) {
 
-	// For pre-checked inputs, show toggled content
+      // Set aria-controls
+      $radio.attr('aria-controls', $dataTarget);
 
-	var target = $('input:checked').parent().attr('data-target');
+      // Set aria-expanded and aria-hidden
+      $radio.attr('aria-expanded', 'false');
+      $('#'+$dataTarget).attr('aria-hidden', 'true');
 
-	$('#'+target).show();
+      // For radio buttons revealing hidden content
+      $radio.on('click', function() {
 
-	}
+        var state = $(this).attr('aria-expanded') === 'false' ? true : false;
 
+        // Toggle hidden content
+        $('#'+$dataTarget).toggle();
 
-	/* ERRORS MOVE TO RELATED FORM ENTRY */
+        // Update aria-expanded and aria-hidden attributes
+        $(this).attr('aria-expanded', state);
+        $('#'+$dataTarget).attr('aria-hidden', !state);
 
-	$('.validation-list a[href^="#"]').on('click', function(e) {
+      });
+    }
+
+    // If the data-target attribute is undefined for a radio button,
+    // hide visible data-target content for radio buttons in the same group
+    else {
+
+      $radio.on('click', function() {
+
+        // Select radio buttons in the same group
+        $(".block-label input[name=" + $radioGroupName + "]").each(function() {
+
+          var groupDataTarget = $(this).parent().attr('data-target');
+
+          // Hide toggled content
+          $('#'+groupDataTarget).hide();
+
+          // Update aria-expanded and aria-hidden attributes
+          if ($(this).attr('aria-controls')) {
+            $(this).attr('aria-expanded', 'false');
+          }
+          $('#'+groupDataTarget).attr('aria-hidden', 'true');
+
+        });
+
+      });
+    }
+
+  });
+
+}
+
+function toggleTabs() {
+
+  if ($('.tabs').length>0) {
+
+    $('.tabs').tabs();
+
+  }
+
+}
+
+function formValidation() {
+
+  $('.validation-list a[href^="#"]').on('click', function(e) {
 
 		e.preventDefault();
 
@@ -71,14 +136,31 @@ $(document).ready(function() {
 
 	});
 
+}
 
-  /* TABS */
 
-  if ($('.tabs').length>0) {
 
-    $('.tabs').tabs();
+$(document).ready(function() {
 
-  }
+  // Turn off jQuery animation
+  jQuery.fx.off = true;
 
+  // Use GOV.UK selection-buttons.js to set selected
+  // and focused states for block labels
+  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']");
+
+  GOVUK.selectionButtons($blockLabels);
+
+  // Details/summary polyfill
+  // See /javascripts/vendor/details.polyfill.js
+
+  // Where .block-label uses the data-target attribute
+  // to toggle hidden content
+
+  showHideCheckboxToggledContent();
+  showHideRadioToggledContent();
+
+  toggleTabs();
+  formValidation();
 
 });
