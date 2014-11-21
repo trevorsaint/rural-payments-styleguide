63923a36caa32e89922c958c8e62323855f222ef
Mateusz Jamiolkowski <M.Jamiolkowski@kainos.com>
2014-11-21 17:17:00 +0000

    Polishing collapsible

--- javascripts/modules/directives/capdCollapsible.js
+++ javascripts/modules/directives/capdCollapsible.js
@@ -2,40 +2,27 @@ define(function () {
     var capdCollapsible = function () {
         return {
             link: function postLink(scope, iElement, iAttrs) {
-                    var $header  = iElement.find('.collapsible-heading h3');
-                    var $content = iElement.find('.collapsible-content');
+                var $header = iElement.find('.collapsible-heading h3');
+                var $content = iElement.find('.collapsible-content');
 
+                // Add button inside $header
+                $header.wrapInner('<button aria-expanded="false" />');
+                var $button = $header.children('button');
 
+                // Add attributes to collapsible content
+                $content.attr('aria-hidden', 'true');
 
-                    // Create a unique ID
+                // Toggle state
+                $button.on('click', function () {
+                    var state = $button.attr('aria-expanded') === 'false' ? true : false;
 
-
-
-                    // Add button inside $header
-                    $header.wrapInner('<button aria-expanded="false" />');
-                    var $button = $header.children('button');
-
-
-                    // Add attributes to collapsible content
-                    $content.attr('aria-hidden', 'true');
-
-
-                    // Toggle state
-                    $button.on('click', function() {
-
-                        var state = $button.attr('aria-expanded') === 'false' ? true : false;
-
-                        $button.attr('aria-expanded', state);
-                        $content.attr('aria-hidden', !state);
-
-                    });
+                    $button.attr('aria-expanded', state);
+                    $content.attr('aria-hidden', !state);
+                });
             },
-            restrict : 'C',
+            restrict: 'C',
             transclude: false,
-            scope : false,
-            controller: function($scope, $element, $attrs, $transclude){
-
-            }
+            scope: false
         }
     }
 
