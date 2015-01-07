66aa934553ce07b68ca8e3b8540bc64587fe1855
Mateusz Jamiolkowski <M.Jamiolkowski@kainos.com>
2015-01-07 15:51:34 +0100

    Removing postinstall file

--- postinstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export GEM_HOME=$PWD/gems
-
-mkdir -p $GEM_HOME
-gem install compass
-
-$GEM_HOME/bin/compass compile --sass-dir assets/sass --css-dir assets/stylesheets -c config/config.rb
