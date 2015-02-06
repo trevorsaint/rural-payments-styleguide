f6f60a7bc5906acdbd16d066709ae15c4afaf764
Trevor Saint <hello@trevorsaint.com>
2015-02-06 11:07:16 +0000

    Added zoom in and zoom out controls to mapping. Set location to Reading as an example

--- assets/javascripts/map.js
+++ assets/javascripts/map.js
@@ -1,19 +1,46 @@
+// Self evoking JavaScript function
 
-var map = new OpenLayers.Map({
-  div: "map",
-  layers: [new OpenLayers.Layer.OSM()],
-  controls: [
+(function openLayers(){
+
+  var map;
+  var lat  = -0.977852;
+  var lon  = 51.454291;
+  var zoom = 4;
+
+  map = new OpenLayers.Map('map', {
+
+    layers: [
+      new OpenLayers.Layer.OSM()
+    ],
+
+    controls: [
+
       new OpenLayers.Control.Navigation({
-          dragPanOptions: {
-              enableKinetic: true
-          }
+
+        dragPanOptions: {
+          enableKinetic: true
+        }
+
       }),
-      new OpenLayers.Control.Attribution(),
+
       new OpenLayers.Control.Zoom({
-          zoomInId: "customZoomIn",
-          zoomOutId: "customZoomOut"
-      })
-  ],
-  center: [0, 0],
-  zoom: 1
-});
+        zoomInId: 'customZoomIn',
+        zoomOutId: 'customZoomOut'
+      }),
+
+    ],
+
+    center: [0, 0],
+    zoom: zoom
+
+  });
+
+  map.setCenter(
+    new OpenLayers.LonLat(lat, lon).transform(
+        new OpenLayers.Projection("EPSG:4326"),
+        map.getProjectionObject()
+    ), 10
+  );
+
+
+})();
